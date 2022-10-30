8f09216d2558d97ec20f5bce73b83a4e1afe79dd
Mudit Singh <73271298+zexor11@users.noreply.github.com>
2022-10-30 13:04:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,81 @@
+let s;
+let s1;
+setInterval(() => {
+    s1 = "Welcome";
+    document.getElementById('hd').innerHTML = s1;
+}, 2000);
+setInterval(() => {
+
+    s = "Your Favourite Series are Here";
+    document.getElementById('hd').innerHTML = s;
+}, 4000);
+
+/*setInterval(() => {
+    document.body.style.backgroundImage = "url('g2.jpg')";
+}, 2000);
+
+setInterval(() => {
+    document.body.style.backgroundImage = "url('w5.jpg')";
+}, 4000);*/
+
+let sp1;
+let sp11;
+setInterval(() => {
+    sp1 = "Best Series of this genre";
+    document.getElementById('p1').innerHTML = sp1;
+}, 2000);
+
+setInterval(() => {
+    sp11 = "";
+    document.getElementById('p1').innerHTML = sp11;
+}, 4000);
+
+setInterval(() => {
+    sp1 = "Best Series of this genre";
+    document.getElementById('p2').innerHTML = sp1;
+}, 2000);
+
+setInterval(() => {
+    sp11 = "";
+    document.getElementById('p2').innerHTML = sp11;
+}, 4000);
+
+setInterval(() => {
+    sp1 = "Best Series of this genre";
+    document.getElementById('p3').innerHTML = sp1;
+}, 2000);
+
+setInterval(() => {
+    sp11 = "";
+    document.getElementById('p3').innerHTML = sp11;
+}, 4000);
+
+setInterval(() => {
+    sp1 = "Best Series of this genre";
+    document.getElementById('p4').innerHTML = sp1;
+}, 2000);
+
+setInterval(() => {
+    sp11 = "";
+    document.getElementById('p4').innerHTML = sp11;
+}, 4000);
+
+setInterval(() => {
+    sp1 = "Best Series of this genre";
+    document.getElementById('p5').innerHTML = sp1;
+}, 2000);
+
+setInterval(() => {
+    sp11 = "";
+    document.getElementById('p5').innerHTML = sp11;
+}, 4000);
+
+setInterval(() => {
+    sp1 = "Best Series of this genre";
+    document.getElementById('p6').innerHTML = sp1;
+}, 2000);
+
+setInterval(() => {
+    sp11 = "";
+    document.getElementById('p6').innerHTML = sp11;
+}, 4000);
